99eeea6cfb3e91be5f425727d9d28fdd15945915
Alex Koniushenko <alex.koniushenko@gmail.com>
2023-04-27 18:38:06 +0300

    react app added

---
+++ frontend/src/App.js
@@ -0,0 +1,4 @@
+import "./App.css";
+import { PostList } from "./components/PostList";
+export const App = () => <PostList />;
+export default App;
---
+++ frontend/src/components/PostList.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { getAllPosts } from "../service";
+
+export const PostList = () => {
+  const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1);
+  useEffect(() => {
+    getAllPosts(page).then((posts) => setPosts(posts.data));
+  }, [page]);
+
+  const togglePage = () => {
+    setPage(page === 1 ? 2 : 1);
+    console.log(page);
+  };
+
+  return (
+    <>
+      <ul>
+        {posts.length > 0 &&
+          posts.map((post) => (
+            <li key={post.pk}>
+              <h4>{post.title}</h4>
+              <h5>{post.author}</h5>
+              <p>{post.content}</p>
+            </li>
+          ))}
+      </ul>
+      <button onClick={togglePage}>Page {page}</button>
+    </>
+  );
+};
